00aca3b819c9b6841f8f89e65994721af9ec7f89
Tatenda <mazhape98@gmail.com>
2025-01-04 00:00:26 +0200

    initial commit

---
+++ src/test/java/com/example/whatsappbackened/WhatsappbackenedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatsappbackened;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappbackenedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
